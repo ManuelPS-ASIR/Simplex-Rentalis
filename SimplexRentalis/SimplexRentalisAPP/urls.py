205b8f5f130b962c7f04bd8b821ca88c7a6761fa
ManuelPS-ASIR <manuel.ponce.sanchez.alu@iesfernandoaguilar.es>
2024-10-15 19:41:48 +0200

    Filtrado Dinamico

--- SimplexRentalis/SimplexRentalisAPP/urls.py
+++ SimplexRentalis/SimplexRentalisAPP/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from . import views
 from SimplexRentalisAPP.views import PropertyViews
+from SimplexRentalisAPP.views import UserDataView
+
 
 
 urlpatterns = [
     path("", views.index, name="index"),
     
     path("Properties/", PropertyViews.as_view(), name='properties'),
+    path("User/", UserDataView.as_view(), name='user'),
+    path("User/<str:username>/", UserDataView.as_view(), name="user_data"),  # Ruta para capturar el username
+
 ]
--- SimplexRentalis/SimplexRentalisAPP/views.py
+++ SimplexRentalis/SimplexRentalisAPP/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from django.views.generic import ListView
-from SimplexRentalisAPP.models import Property
+from SimplexRentalisAPP.models import Property,User
 # Create your views here.
 class PropertyViews(ListView):
     model = Property
     context_object_name = "propietarios_de_los_hogares"
+    queryset = Property.objects.all()
+    #Puedo usar queryset para traer solo la información necesario e incluso aplicar filtros
+    template_name="SimplexRentalisAPP/property_list.html"
 
+class UserDataView(ListView):
+    template_name = "SimplexRentalisAPP/user_data.html"  # Ruta de la plantilla
+
+    def get_queryset(self):
+        # Captura el username de la URL
+        self.user = get_object_or_404(User, username=self.kwargs["username"])
+        # Filtra las propiedades por el usuario
+        return Property.objects.filter(owner=self.user)
+
+    def get_context_data(self, **kwargs):
+        # Llama a la implementación base para obtener un contexto
+        context = super().get_context_data(**kwargs)
+        # Añade el usuario al contexto para usarlo en la plantilla
+        context["usuario"] = self.user
+        return context
 
 def index(request):
     return HttpResponse("Página Index de SimplesRentalisAPP")
