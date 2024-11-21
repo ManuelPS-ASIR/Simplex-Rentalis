22a8822dc1d260b24166b3aa0b13f28b2c3d5506
ManuelPS-ASIR <manuel.ponce.sanchez.alu@iesfernandoaguilar.es>
2024-11-21 17:53:13 +0100

    manejo de la imagen de portada

--- SimplexRentalis/SimplexRentalisAPP/views.py
+++ SimplexRentalis/SimplexRentalisAPP/views.py
@@ -194,7 +194,7 @@ def agregar_propiedad(request):
         return render(request, 'SimplexRentalisAPP/agregar_propiedad.html', {'imagenes': imagenes})
 
     else:
-        return render(request, 'SimplexRentalisAPP/agregar_propiedad.html')
+        return render(request, 'SimplexRentalisAPP/propiedades_usuario.html')
 
 
 # Vista para agregar imágenes adicionales a una propiedad
