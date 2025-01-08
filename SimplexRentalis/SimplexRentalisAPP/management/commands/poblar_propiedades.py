b812c25cc65d50a8d3459870e0b027772d43c3d9
ManuelPS-ASIR <manuel.ponce.sanchez.alu@iesfernandoaguilar.es>
2025-01-08 11:47:37 +0000

    +3

--- SimplexRentalis/SimplexRentalisAPP/management/commands/poblar_propiedades.py
+++ SimplexRentalis/SimplexRentalisAPP/management/commands/poblar_propiedades.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import csv
 from django.core.management.base import BaseCommand
 from SimplexRentalisAPP.models import Propiedades, User, Galeria, Direcciones
@@ -12,21 +13,23 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         csv_file = kwargs['csv_file']
-        images_folder = 'static/comand_admin/fotos2'
+        #images_folder = os.path.join(os.getcwd(), 'static/comand_admin/fotos2')  # Ruta absoluta
+        images_folder = os.path.join(settings.STATICFILES_DIRS[0], 'comand_admin', 'fotos2')  # Ruta absoluta
 
         # Leer el archivo CSV con delimitador explícito
         try:
             with open(csv_file, newline='', encoding='utf-8') as csvfile:
                 reader = csv.DictReader(csvfile, delimiter=',')
-                
+
                 # Validar que el CSV tiene las columnas esperadas
-                expected_columns = [
+                expected_columns = {
                     'Nombre', 'Descripcion', 'Direccion', 'Precio', 'Propietario',
                     'Calificacion', 'Pets', 'Mantenimiento', 'Huespedes', 'Banos', 'Dormitorios', 'Imagenes'
-                ]
-                if reader.fieldnames != expected_columns:
+                }
+                if set(reader.fieldnames) != expected_columns:
                     self.stdout.write(self.style.ERROR(
-                        f"Las columnas del CSV no coinciden con las esperadas: {expected_columns}. Actual: {reader.fieldnames}."
+                        f"Las columnas del CSV no coinciden con las esperadas. "
+                        f"Esperado: {expected_columns}, Actual: {reader.fieldnames}."
                     ))
                     return
 
@@ -35,36 +38,64 @@ class Command(BaseCommand):
                         # Buscar el usuario propietario
                         propietario = User.objects.filter(username=row['Propietario']).first()
                         if not propietario:
-                            self.stdout.write(self.style.ERROR(f'Propietario {row["Propietario"]} no encontrado. Omitiendo propiedad {row["Nombre"]}.'))
+                            self.stdout.write(self.style.ERROR(
+                                f'Propietario {row["Propietario"]} no encontrado. Omitiendo propiedad {row["Nombre"]}.'
+                            ))
                             continue
                         
                         # Dividir la dirección
                         direccion_completa = row['Direccion'].split(',')
                         if len(direccion_completa) != 6:
-                            self.stdout.write(self.style.ERROR(f'Dirección mal formada para la propiedad {row["Nombre"]}. Omitiendo. Dirección: {row["Direccion"]}'))
+                            self.stdout.write(self.style.ERROR(
+                                f'Dirección mal formada para la propiedad {row["Nombre"]}. Omitiendo. Dirección: {row["Direccion"]}'
+                            ))
                             continue
                         
-                        calle, numero_casa, codigo_postal, ciudad, co_autonoma, provincia = [part.strip() for part in direccion_completa]
+                        calle, numero_casa, codigo_postal, ciudad, co_autonoma, provincia = [
+                            part.strip() for part in direccion_completa
+                        ]
 
                         # Crear la dirección
                         try:
-                            direccion_instance = Direcciones.objects.create(
+                            direccion_instance, created = Direcciones.objects.get_or_create(
                                 calle=calle,
                                 numero_casa=numero_casa,
                                 codigo_postal=codigo_postal,
                                 ciudad=ciudad,
                                 co_autonoma=co_autonoma,
                                 provincia=provincia,
-                                pais="España"  # Asumiendo que el país siempre es España para este contexto
+                                pais="España"
                             )
                         except ValidationError as e:
-                            self.stdout.write(self.style.ERROR(f'Error al crear la dirección para la propiedad {row["Nombre"]}: {e.messages}. Dirección: {row["Direccion"]}. Omitiendo.'))
+                            self.stdout.write(self.style.ERROR(
+                                f'Error al crear la dirección para la propiedad {row["Nombre"]}: {e.messages}. '
+                                f'Dirección: {row["Direccion"]}. Omitiendo.'
+                            ))
                             continue
 
                         # Verificar si ya existe una propiedad con la misma dirección y propietario
                         if Propiedades.objects.filter(direccion=direccion_instance, propietario=propietario).exists():
-                            self.stdout.write(self.style.ERROR(f'Propiedad con dirección {direccion_instance} y propietario {propietario} ya existe. Omitiendo {row["Nombre"]}.'))
+                            self.stdout.write(self.style.ERROR(
+                                f'Propiedad con dirección {direccion_instance} y propietario {propietario} ya existe. '
+                                f'Omitiendo {row["Nombre"]}.'
+                            ))
                             continue
+
+                        # Validar imágenes
+                        galeria_prefix = row['Imagenes']
+                        imagenes_encontradas = glob.glob(os.path.join(images_folder, f"{galeria_prefix}*.png"))
+
+                        if len(imagenes_encontradas) < 5:
+                            self.stdout.write(self.style.ERROR(
+                                f'No se encontraron al menos 5 imágenes para la propiedad {row["Nombre"]} '
+                                f'con prefijo {galeria_prefix}. Omitiendo. Ruta buscada: {images_folder}'
+                            ))
+                            continue
+                        else:
+                            self.stdout.write(self.style.SUCCESS(
+                                f"Se encontraron {len(imagenes_encontradas)} imágenes para la propiedad {row['Nombre']} "
+                                f"en las rutas: {', '.join(imagenes_encontradas)}"
+                            ))
 
                         # Crear la propiedad
                         try:
@@ -82,25 +113,37 @@ class Command(BaseCommand):
                                 cantidad_dormitorios=int(row['Dormitorios']),
                             )
                         except Exception as e:
-                            self.stdout.write(self.style.ERROR(f'Error al crear la propiedad {row["Nombre"]}: {e}. Datos: {row}. Omitiendo.'))
+                            self.stdout.write(self.style.ERROR(
+                                f'Error al crear la propiedad {row["Nombre"]}: {e}. Datos: {row}. Omitiendo.'
+                            ))
                             continue
 
                         # Asociar imágenes a la propiedad y marcar la primera como portada
-                        galeria_prefix = row['Imagenes']
                         primera_imagen = True
-                        for filename in os.listdir(images_folder):
-                            if filename.startswith(galeria_prefix):
-                                image_path = os.path.join(images_folder, filename)
+                        for image_path in imagenes_encontradas:
+                            try:
                                 Galeria.objects.create(
                                     propiedad=propiedad,
-                                    imagen=image_path,
+                                    imagen=os.path.relpath(image_path, images_folder),
                                     portada=primera_imagen
                                 )
+                                self.stdout.write(self.style.SUCCESS(
+                                    f"Imagen {os.path.relpath(image_path, images_folder)} asociada exitosamente a la propiedad {row['Nombre']}."
+                                ))
                                 primera_imagen = False
+                            except Exception as e:
+                                self.stdout.write(self.style.ERROR(
+                                    f'Error al asociar imagen {image_path} con la propiedad {row["Nombre"]}: {e}'
+                                ))
+                                continue
 
-                        self.stdout.write(self.style.SUCCESS(f'Propiedad {propiedad.nombre} creada con éxito con ID {propiedad.id}.'))
+                        self.stdout.write(self.style.SUCCESS(
+                            f'Propiedad {propiedad.nombre} creada con éxito con ID {propiedad.id}.'
+                        ))
                     except Exception as e:
-                        self.stdout.write(self.style.ERROR(f'Error procesando la propiedad {row["Nombre"]}: {e}. Datos: {row}. Omitiendo.'))
+                        self.stdout.write(self.style.ERROR(
+                            f'Error procesando la propiedad {row["Nombre"]}: {e}. Datos: {row}. Omitiendo.'
+                        ))
         except FileNotFoundError:
             self.stdout.write(self.style.ERROR(f"El archivo {csv_file} no se encontró. Verifica la ruta."))
         except Exception as e:
