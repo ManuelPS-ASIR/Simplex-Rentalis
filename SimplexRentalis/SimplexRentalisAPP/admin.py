d32e125dd19f278435c13acda409f96a7454372e
ManuelPS-ASIR <manuel.ponce.sanchez.alu@iesfernandoaguilar.es>
2025-01-08 11:46:52 +0000

    +2

--- SimplexRentalis/SimplexRentalisAPP/admin.py
+++ SimplexRentalis/SimplexRentalisAPP/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Propiedades, ReservaPersona, Reservas, IdentidadReserva, Opiniones, IdentidadUsuario
+from .models import User, Propiedades, ReservaPersona, Reservas, IdentidadReserva, Opiniones, IdentidadUsuario, Direcciones
 admin.site.register(User)
 admin.site.register(Propiedades)
 admin.site.register(ReservaPersona)
@@ -9,3 +9,4 @@ admin.site.register(Reservas)
 admin.site.register(IdentidadReserva)
 admin.site.register(IdentidadUsuario)
 admin.site.register(Opiniones)
+admin.site.register(Direcciones)
